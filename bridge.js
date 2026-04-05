e2397f17b16385193bed6bc30f13ed9a22055b1d
huynq2k5 <huyn84341@gmail.com>
2026-04-05 11:53:23 +0700

    sua mail lan 3

--- bridge.js
+++ bridge.js
@@ -21,8 +21,10 @@ const cauHinhEmail = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
+    
+    family: 4, 
     tls: {
-        rejectUnauthorized: false 
+        rejectUnauthorized: false
     }
 });
 
