27d7ced7ad75164a463bd4531a8091f0b11ac8c5
huynq2k5 <huyn84341@gmail.com>
2026-04-05 12:04:13 +0700

    sua mail lan 5

--- bridge.js
+++ bridge.js
@@ -16,16 +16,16 @@ const ketNoiDb = mysql.createPool({
 });
 
 const cauHinhEmail = nodemailer.createTransport({
-    host: '74.125.204.108',
+    host: 'smtp.gmail.com', 
     port: 465,
     secure: true, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    
-    connectionTimeout: 10000, 
-    greetingTimeout: 10000,
+    family: 4, 
+    connectionTimeout: 20000,
+    greetingTimeout: 20000,
     tls: {
         rejectUnauthorized: false
     }
